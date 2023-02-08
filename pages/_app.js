831dcd2d1b3d30287d0d173bf49d466ab7f186ef
Ryan Kretch <ryan.kretch@optibus.com>
2023-02-08 09:51:02 +0200

    Add loader

--- pages/_app.js
+++ pages/_app.js
@@ -11,18 +11,34 @@ import siteMetadata from '@/data/siteMetadata'
 import { Analytics } from 'pliny/analytics'
 import { SearchProvider } from 'pliny/search'
 import LayoutWrapper from '@/components/LayoutWrapper'
+import { useState, useEffect } from 'react'
+
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 500)
+  }, [])
+
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <Analytics analyticsConfig={siteMetadata.analytics} />
-      <LayoutWrapper>
-        <SearchProvider searchConfig={siteMetadata.search}>
-          <Component {...pageProps} />
-        </SearchProvider>
-      </LayoutWrapper>
-    </ThemeProvider>
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+          <Head>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+          </Head>
+          <Analytics analyticsConfig={siteMetadata.analytics} />
+          <LayoutWrapper>
+            <SearchProvider searchConfig={siteMetadata.search}>
+              <Component {...pageProps} />
+            </SearchProvider>
+          </LayoutWrapper>
+        </ThemeProvider>
+      )}
+    </>
   )
 }
